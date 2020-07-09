f3bebb15c40719fc257b426e9ca7dd24ba7b6022
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-07-09 07:58:29 -0600

    Adds HandController Actor and VRPawn

---
+++ LightPainter/Source/LightPainter/VRPawn.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRPawn.h"
+#include "Engine/World.h"
+#include "HandController.h"
+
+// Sets default values
+AVRPawn::AVRPawn()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
+	SetRootComponent(VRRoot);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VRRoot);
+
+}
+
+// Called when the game starts or when spawned
+void AVRPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HandControllerClass) {
+		RightHandController = GetWorld()->SpawnActor<AHandController>(HandControllerClass);
+		RightHandController->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::SnapToTargetIncludingScale);
+	}
+	
+}
+
+
