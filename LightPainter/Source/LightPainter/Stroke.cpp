d36e026248ac874eecb3834ef0ac5d862fbda057
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-07-10 08:15:32 -0600

    Paint with splines logic

--- LightPainter/Source/LightPainter/Stroke.cpp
+++ LightPainter/Source/LightPainter/Stroke.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Stroke.h"
+#include "Components/SplineMeshComponent.h"
 
 // Sets default values
 AStroke::AStroke()
@@ -9,19 +10,29 @@ AStroke::AStroke()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-}
-
-// Called when the game starts or when spawned
-void AStroke::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AStroke::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
 
 }
 
+void AStroke::Update(FVector CursorLocation) {
+	USplineMeshComponent* Spline = CreateSplineMesh();
+
+	FVector StartPosition = GetActorTransform().InverseTransformPosition(CursorLocation);
+	FVector EndPosition = GetActorTransform().InverseTransformPosition(PreviousCursorLocation);
+
+	Spline->SetStartAndEnd(StartPosition, FVector::ZeroVector, EndPosition, FVector::ZeroVector);
+
+	PreviousCursorLocation = CursorLocation;
+}
+
+USplineMeshComponent* AStroke::CreateSplineMesh() {
+	USplineMeshComponent* NewSpline = NewObject<USplineMeshComponent>(this);
+	NewSpline->SetMobility(EComponentMobility::Movable);
+	NewSpline->AttachToComponent(Root, FAttachmentTransformRules::SnapToTargetIncludingScale);
+	NewSpline->SetStaticMesh(SplineMesh);
+	NewSpline->SetMaterial(0, SplineMaterial);
+	NewSpline->RegisterComponent();
+
+	return NewSpline;
+}
--- LightPainter/Source/LightPainter/Stroke.h
+++ LightPainter/Source/LightPainter/Stroke.h
@@ -15,12 +15,23 @@ public:
 	// Sets default values for this actor's properties
 	AStroke();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	void Update(FVector CursorLocation);
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
 
+	class USplineMeshComponent* CreateSplineMesh();
+
+	// Components
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+
+	// Config
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMesh* SplineMesh;
+
+	UPROPERTY(EditDefaultsOnly)
+	UMaterialInterface* SplineMaterial;
+
+	// State
+	FVector PreviousCursorLocation;
 };
