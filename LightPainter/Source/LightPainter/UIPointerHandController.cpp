7a1c00330d7739653c0047a71150280cf2cda0b3
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-07-16 08:00:08 -0600

    Adds pointer to UI Hand Controller

--- LightPainter/Source/LightPainter/UIPointerHandController.cpp
+++ LightPainter/Source/LightPainter/UIPointerHandController.cpp
@@ -3,3 +3,17 @@
 
 #include "UIPointerHandController.h"
 
+#include "InputCoreTypes.h"
+
+AUIPointerHandController::AUIPointerHandController() {
+	Pointer = CreateDefaultSubobject<UWidgetInteractionComponent>(TEXT("Pointer"));
+	Pointer->SetupAttachment(GetRootComponent());
+}
+
+void AUIPointerHandController::TriggerPressed() {
+	Pointer->PressPointerKey(EKeys::LeftMouseButton);
+}
+
+void AUIPointerHandController::TriggerReleased() {
+	Pointer->ReleasePointerKey(EKeys::LeftMouseButton);
+}
