2e8c65782e9c34a0b59186775e242616099d4a56
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-07-24 07:56:28 -0600

    Iterate all saved games in log

--- LightPainter/Source/LightPainter/Saving/PainterSaveGame.cpp
+++ LightPainter/Source/LightPainter/Saving/PainterSaveGame.cpp
@@ -6,16 +6,28 @@
 #include "Kismet/GameplayStatics.h"
 #include "EngineUtils.h"
 #include "Misc/Guid.h"
+#include "PainterSaveGameIndex.h"
 
 #include "../Stroke.h"
 
 UPainterSaveGame* UPainterSaveGame::Create() {
 	UPainterSaveGame* NewSaveGame = Cast<UPainterSaveGame>(UGameplayStatics::CreateSaveGameObject(StaticClass()));
 	NewSaveGame->SlotName = FGuid::NewGuid().ToString();
+	if (!NewSaveGame->Save()) return nullptr;
+
+	UPainterSaveGameIndex* Index = UPainterSaveGameIndex::Load();
+	Index->AddSaveGame(NewSaveGame);
+	Index->Save();
+
 	return NewSaveGame;
 }
 
 bool UPainterSaveGame::Save() {
+	UE_LOG(LogTemp, Warning, TEXT("Painting Index:"));
+	// In the lecture it was called SlotName not SlotNameItem but it caused error C4458.
+	for (FString SlotNameItem : UPainterSaveGameIndex::Load()->GetSlotNames()) {
+		UE_LOG(LogTemp, Warning, TEXT("Painting name: %s"), *SlotNameItem);
+	}
 	return UGameplayStatics::SaveGameToSlot(this, SlotName, 0);
 }
 
