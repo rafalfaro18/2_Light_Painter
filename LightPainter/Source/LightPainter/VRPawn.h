ef607c66996daff7db7f3ae4d382652cbc83d161
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-07-31 08:33:34 -0600

    Loads saved canvas games

--- LightPainter/Source/LightPainter/VRPawn.h
+++ LightPainter/Source/LightPainter/VRPawn.h
@@ -28,7 +28,6 @@ private:
 	void RightTriggerReleased() { if (RightPaintBrushHandController) RightPaintBrushHandController->TriggerReleased(); }
 
 	void Save();
-	void Load();
 	
 	// Config
 	UPROPERTY(EditDefaultsOnly)
