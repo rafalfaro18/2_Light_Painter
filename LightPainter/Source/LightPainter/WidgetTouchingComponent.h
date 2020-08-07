ab2cfb98940523de12e6ba9e3c4fc631c897be53
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-08-07 08:06:14 -0600

    Adds new class based on Widget Interaction Component

---
+++ LightPainter/Source/LightPainter/WidgetTouchingComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetInteractionComponent.h"
+#include "WidgetTouchingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTPAINTER_API UWidgetTouchingComponent : public UWidgetInteractionComponent
+{
+	GENERATED_BODY()
+	
+};
